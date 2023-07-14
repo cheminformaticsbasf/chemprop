a31b4826a083532fc14abf430fce509c60bd5f3d
HemmerJ3 <jennifer.hemmerich@basf.com>
2023-07-14 11:49:19 +0200

    combine v2fix with master

---
+++ chemprop/v2/models/__init__.py
@@ -0,0 +1,19 @@
+from .modules import (
+    MessagePassingBlock,
+    MolecularMessagePassingBlock,
+    MolecularInput,
+    CompositeMessagePassingBlock,
+    AtomMessageBlock,
+    BondMessageBlock,
+)
+from .models import (
+    MPNN,
+    ClassificationMPNN,
+    BinaryClassificationMPNN,
+    DirichletClassificationMPNN,
+    MulticlassMPNN,
+    DirichletMulticlassMPNN,
+    RegressionMPNN,
+    MveRegressionMPNN,
+    SpectralMPNN,
+)
